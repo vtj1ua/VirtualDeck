c26e0bf25094cae9de951e738e563d698fd887b5
CarloHSpairani <chs17@gcloud.ua.es>
2021-11-29 18:12:32 +0100

    nuevo

---
+++ VirtualDeckWeb/Models/TokenPackViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace VirtualDeckWeb.Models
+{
+    public class TokenPackViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public string Img { get; set; }
+        public int Tokens { get; set; }
+    }
+}
