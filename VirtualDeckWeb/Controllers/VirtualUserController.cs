b8f63f50761a0da1037c15444bd2d8eb85531754
Javier Marin Ubeda <jmu3@mscloud.ua.es>
2021-11-29 20:31:20 +0100

    virtualuser_edit_casi(falla el modify)

--- VirtualDeckWeb/Controllers/VirtualUserController.cs
+++ VirtualDeckWeb/Controllers/VirtualUserController.cs
@@ -8,6 +8,7 @@ using VirtualDeckGenNHibernate.EN.VirtualDeck;
 using VirtualDeckGenNHibernate.CAD.VirtualDeck;
 using VirtualDeckWeb.Assemblers;
 using VirtualDeckWeb.Models;
+using VirtualDeckGenNHibernate.Enumerated.VirtualDeck;
 
 namespace VirtualDeckWeb.Controllers
 {
@@ -35,6 +36,53 @@ namespace VirtualDeckWeb.Controllers
             VirtualUserViewModel model = new VirtualUserAssembler().ConvertENToModelUI(virtualUserEN);
             SessionClose();
             return View(model);
+        }
+
+        public ActionResult Edit()
+        {
+            SessionInitialize();
+            VirtualUserCAD virtualUserCAD = new VirtualUserCAD(session);
+            VirtualUserCEN virtualUserCEN = new VirtualUserCEN(virtualUserCAD);
+            VirtualUserEN en = Session["user"] as VirtualUserEN;
+            if(en != null)
+            {
+                VirtualUserEN virtualUserEN = virtualUserCEN.ReadOID(en.Id);
+                VirtualUserViewModel model = new VirtualUserAssembler().ConvertENToModelUI(virtualUserEN);
+                SessionClose();
+                return View(model);
+            }
+            SessionClose();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(VirtualUserViewModel usu)
+        {
+            try
+            {
+                VirtualUserCEN cen = new VirtualUserCEN();
+                switch (usu.CombatStatus)
+                {
+                    case 1:
+                        cen.Modify(usu.Id, usu.Pass, usu.UserName, usu.Email, usu.Description, usu.Tokens, usu.Img, CombatStatusEnum.Inactive);
+                        break;
+                    case 2:
+                        cen.Modify(usu.Id, usu.Pass, usu.UserName, usu.Email, usu.Description, usu.Tokens, usu.Img, CombatStatusEnum.Searching);
+                        break;
+                }
+
+                IList<VirtualUserEN> vicen = cen.UsersByEmail(usu.Email);
+                if (vicen.Count > 0)
+                {
+                    Session["user"] = vicen[0];
+                }
+
+                return RedirectToAction("Details", new { id = usu.Id });
+            }
+            catch
+            {
+                return View();
+            }
         }
     }
 }
