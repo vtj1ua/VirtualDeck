c7d0f5cc370053beebc0f3f4b2f372cac480e515
CarloHSpairani <chs17@gcloud.ua.es>
2021-12-18 11:01:19 +0100

    Carlo Js onchange cantidad

--- VirtualDeckWeb/Scripts/script.js
+++ VirtualDeckWeb/Scripts/script.js
@@ -30,7 +30,17 @@ function borrarFoto(dv) {
 function changeTokens(input) {
 	let price = parseInt(document.querySelector('.price').textContent);
 	let amount = parseInt(input.value);
-	let total = price * amount;
+	let total = price;
+	if (amount >= 1) {
+		if (amount > 50) {
+			amount = 50;
+			input.value = 50;
+		}
+		total = price * amount;
+	} else {
+		input.value = 1;
+	}
 	document.querySelector('.totalPrice').innerText = total;
+
 	return false;
 }
