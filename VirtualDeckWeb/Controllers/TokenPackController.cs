8d6cfcaf2a21475e3ab171ae5bf640351816c5be
Victor <vtj1@alu.ua.es>
2021-12-20 17:34:27 +0100

    Estilo formularios

--- VirtualDeckWeb/Controllers/TokenPackController.cs
+++ VirtualDeckWeb/Controllers/TokenPackController.cs
@@ -45,6 +45,7 @@ namespace VirtualDeckWeb.Controllers
 
                 tokenPackCP.PurchaseTokenPack(model.Id, virtualUserEN.Id);
 
+                virtualUserEN.Tokens += model.Tokens;
                 TempData["OperationResult"] = new OperationResultViewModel(ModalMessageType.Success, "Compra realizada", 
                     "Tu compra de '" + model.Name + "' se ha realizado correctamente");
                 return RedirectToAction("Details", "VirtualUser", new { id = virtualUserEN.Id });
